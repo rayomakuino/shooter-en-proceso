e2ea44132539921f1536ec1e04d93620ad64de29
rayomakuino <adamgjg2010@gmail.com>
2026-05-26 17:49:50 -0400

    version 1

---
+++ shooter_game.py
@@ -0,0 +1,225 @@
+from pygame import *
+from random import *
+from config import *
+from time import time as timer
+
+init()
+font.init()
+#mixer.init()
+
+# parametros iniciales
+vidas = 5
+puntos = 0
+fallos = 0
+num_balas = 0
+
+
+
+# SONIDOS
+# Musica de fondo
+#mixer.music.load(BGM)
+#mixer.music.play()
+
+#laser_sfx = mixer.Sound(FIRE_FX)
+
+
+# CONSTANTES Y VARIABLES DE INICIO-
+
+
+# clases
+class GameSprite(sprite.Sprite):
+    def __init__(self, sprite_img, cord_x, cord_y, sprite_width, sprite_height, speed):
+        super().__init__()
+        self.width = sprite_width
+        self.height = sprite_height
+        self.image = transform.scale(image.load(sprite_img), (self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rect.x = cord_x
+        self.rect.y = cord_y
+        self.speed = speed
+
+    def reset(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x >= 0:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x <= ANCHO - self.width:
+            self.rect.x += self.speed
+        elif keys [K_w] and self.rect.y >=0:
+            self.rect.y -= self.speed
+        elif keys [K_s] and self.rect.y <= ALTO - self.rect.width:
+            self.rect.y += self.speed
+
+    def fire(self):
+        bala = Bullet(BULLET_IMG, self.rect.centerx - 5, self.rect.top, 10, 15, 5)
+        balas.add(bala)
+        # laser_sfx.play()
+
+class Enemy(GameSprite):
+    def update(self):
+        global fallos
+        self.rect.y += self.speed
+        if self.rect.y >= ALTO: # El enemigo cruza el borde inferior de la pantalla
+            self.rect.y = -self.height
+            self.rect.x = randint(0, ANCHO - 60)
+            self.speed = randint(1, 4)
+            fallos += 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y <= -15:
+            self.kill() # Remuevo la instancia del juego
+
+class Wall(sprite.Sprite):
+    def __init__(self, color, x, y, width, height):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.color = color
+
+        self.image = Surface([self.width, self.height])
+        self.image.fill(self.color)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+# Objetos
+window = display.set_mode((ANCHO, ALTO))
+display.set_caption(TITULO)
+
+# Trabajo con fuentes
+font_1 = font.Font(None, 24)
+font_2 = font.Font(None, 60)
+
+goal = GameSprite((GOALD_IMG), 300, 50, 90, 90, 5)
+player = Player(PLAYER_IMG, (ANCHO - 60) // 2, ALTO - 60, 60, 60, 5)
+balas = sprite.Group()
+aliens = sprite.Group()
+asteroids = sprite.Group()
+
+for i in range(5):
+    enemy = Enemy(ENEMY_IMG, randint(0, ANCHO - 60), -60, 80, 60, randint(1, 4))
+    aliens.add(enemy)
+
+for i in range (3):
+    rock = Enemy(ROCK_IMG, randint(0, ANCHO - 60), -40, 40, 40, randint(3, 7))
+    asteroids.add(rock)
+
+# fondo
+background = transform.scale(image.load(BACKGROUND_IMG), (ANCHO, ALTO))
+
+
+run = True
+finish = False
+reloading = False
+clock = time.Clock()
+
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                if num_balas < 5 and reloading == False:
+                    player.fire()
+                    num_balas += 1
+                if num_balas >= 5 and reloading == False:
+                    reloading = True
+                    timer_start = timer() # marcamos el tiempo
+
+            if e.key == K_r:
+                finish = False
+                vidas = 5
+                puntos = 0
+                fallos = 0
+                num_balas = 0
+
+
+    if not finish:
+        window.fill(BACK_COLOR)
+        window.blit(background, (0, 0))
+        puntos_txt = font_1.render(f'PUNTOS: {puntos}', 1, WHITE)
+        fallos_txt = font_1.render(f'FALLOS: {fallos}', 1, WHITE)
+        vidas_txt = font_2.render(f'{vidas}', 1, GREEN)
+        window.blit(puntos_txt, (40, 40))
+        window.blit(fallos_txt, (40, 70))
+        window.blit(vidas_txt, (550, 40))
+
+
+        player.reset(window)
+        goal.reset(window)
+        player.update()
+        aliens.draw(window)
+        aliens.update()
+        asteroids.draw(window)
+        asteroids.update()
+        balas.draw(window)
+        balas.update()
+
+        # Mecanica de recarga
+        if reloading:
+            timer_finish = timer()
+
+            if timer_finish - timer_start < 1:
+                reload_text = font_1.render('RECARGANDO...', 1, YELLOW)
+                window.blit(reload_text, (150, ALTO // 2))
+            else:
+                num_balas = 0
+                reloading = False
+
+
+        obst = sprite.groupcollide(balas, asteroids, True, False)
+
+        collision = sprite.groupcollide(balas, aliens, True, True)
+        for c in collision:
+            puntos += 1
+            enemy = Enemy(ENEMY_IMG, randint(0, ANCHO - 60), -60, 80, 60, randint(1, 4))
+            aliens.add(enemy)
+
+        if sprite.collide_rect(player, goal):
+            window.fill(BLACK)
+            you_win = transform.scale(image.load(YOU_WIN_IMG), (ANCHO, ALTO))
+            window.blit(you_win, (0, 0))
+
+            finish = True
+
+        if sprite.spritecollide(player, aliens, True):
+            vidas -= 1
+            enemy = Enemy(ENEMY_IMG, randint(0, ANCHO - 60), -60, 80, 60, randint(1, 4))
+            aliens.add(enemy)
+
+        # CONDICION DE DERROTA:
+        if fallos >= 10 or vidas <= 0 or sprite.spritecollide(player, asteroids, True):
+            finish = True
+            window.fill(BLACK)
+            game_over = transform.scale(image.load(GAMEOVER_IMG), (ANCHO, ALTO))
+            window.blit(game_over, (0, 0))
+            restart_txt = font_1.render(f'"R" para reiniciar!', 1, WHITE)
+            window.blit(restart_txt, (180, 420))
+
+        # CONDICION DE VICTORIA
+        if puntos == 20:
+            finish = True
+            window.fill(WHITE)
+            # RENDERIZAR IMAGEN DE VICTORIA
+
+
+
+
+    # if finish:
+    #     mixer.music.stop()
+
+    # NO TOCAR
+    display.update()
+    clock.tick(FPS)
+quit()
+
+
+
+
+
